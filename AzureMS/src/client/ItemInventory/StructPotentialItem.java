56a58b315126f8960352bb5dc057584763769496
Bratah123 <brandonnguyen3301@gmail.com>
2020-12-24 16:00:06 -0800

    fixed All stat and crit dmg not being handled in Potential Strings

--- AzureMS/src/client/ItemInventory/StructPotentialItem.java
+++ AzureMS/src/client/ItemInventory/StructPotentialItem.java
@@ -9,6 +9,8 @@
 
 package client.ItemInventory;
 
+import client.Character.MapleCharacter;
+
 public class StructPotentialItem {
 
     public byte incSTR, incDEX, incINT, incLUK, incACC, incEVA, incSpeed, incJump,
@@ -24,18 +26,35 @@ public class StructPotentialItem {
     public short incMHP, incMMP, attackType, skillID;
     public int optionType, reqLevel, weight, potentialID; //probably the slot
     public String face, string; //angry, cheers, love, blaze, glitter
+    public boolean isAllStat;
 
     @Override
     public final String toString() {
         final StringBuilder ret = new StringBuilder();
+        if(incAsrR > 0)
+        {
+            ret.append("Status Resistance ");
+            ret.append(incAsrR);
+            ret.append("%");
+            ret.append(" ");
+        }
+        if(incTerR > 0)
+        {
+            ret.append("Status Resistance ");
+            ret.append(incTerR);
+            ret.append("%");
+            ret.append(" ");
+        }
         if (incMesoProp > 0) {
-            ret.append("Gives MESO(not coded): ");
+            ret.append("Meso Drop Rate ");
             ret.append(incMesoProp);
+            ret.append("%");
             ret.append(" ");
         }
         if (incRewardProp > 0) {
-            ret.append("Gives ITEM(not coded): ");
+            ret.append("Item Drop Rate ");
             ret.append(incRewardProp);
+            ret.append("%");
             ret.append(" ");
         }
         if (prop > 0) {
@@ -64,7 +83,7 @@ public class StructPotentialItem {
             ret.append(" ");
         }
         if (boss) {
-            ret.append("BOSS ONLY, ");
+            ret.append("Boss Damage: ");
         }
         if (face.length() > 0) {
             ret.append("Face Expression: ");
@@ -87,8 +106,9 @@ public class StructPotentialItem {
             ret.append(" ");
         }
         if (ignoreTargetDEF > 0) {
-            ret.append("Ignores Monster DEF %: ");
+            ret.append("Ignore Monster DEF ");
             ret.append(ignoreTargetDEF);
+            ret.append("%");
             ret.append(" ");
         }
         if (RecoveryHP > 0) {
@@ -117,57 +137,60 @@ public class StructPotentialItem {
             ret.append(" ");
         }
         if (ignoreDAM > 0) {
-            ret.append("Ignores Monster Damage: ");
+            ret.append("Ignores Monster Damage +");
             ret.append(ignoreDAM);
             ret.append(" ");
         }
         if (ignoreDAMr > 0) {
-            ret.append("Ignores Monster Damage %: ");
+            ret.append("Ignores Monster Damage");
             ret.append(ignoreDAMr);
+            ret.append("%");
             ret.append(" ");
         }
         if (incMHP > 0) {
-            ret.append("Gives HP: ");
+            ret.append("HP +");
             ret.append(incMHP);
             ret.append(" ");
         }
         if (incMMP > 0) {
-            ret.append("Gives MP: ");
+            ret.append("MP +");
             ret.append(incMMP);
             ret.append(" ");
         }
         if (incMHPr > 0) {
-            ret.append("Gives HP %: ");
+            ret.append("HP ");
             ret.append(incMHPr);
+            ret.append("%");
             ret.append(" ");
         }
         if (incMMPr > 0) {
-            ret.append("Gives MP %: ");
+            ret.append("MP ");
             ret.append(incMMPr);
+            ret.append("%");
             ret.append(" ");
         }
-        if (incSTR > 0) {
-            ret.append("Gives STR: ");
+        if (incSTR > 0 && incLUK <= 0 && incDEX <= 0 && incINT <= 0) {
+            ret.append("STR +");
             ret.append(incSTR);
             ret.append(" ");
         }
-        if (incDEX > 0) {
-            ret.append("Gives DEX: ");
+        if (incDEX > 0 && incLUK <= 0 && incSTR <= 0 && incINT <= 0) {
+            ret.append("DEX +");
             ret.append(incDEX);
             ret.append(" ");
         }
-        if (incINT > 0) {
-            ret.append("Gives INT: ");
+        if (incINT > 0 && incLUK <= 0 && incDEX <= 0 && incSTR <= 0) {
+            ret.append("INT +");
             ret.append(incINT);
             ret.append(" ");
         }
-        if (incLUK > 0) {
-            ret.append("Gives LUK: ");
+        if (incLUK > 0 && incSTR <= 0 && incDEX <= 0 && incINT <= 0) {
+            ret.append("LUK +");
             ret.append(incLUK);
             ret.append(" ");
         }
         if (incACC > 0) {
-            ret.append("Gives ACC: ");
+            ret.append("ACC +");
             ret.append(incACC);
             ret.append(" ");
         }
@@ -177,58 +200,63 @@ public class StructPotentialItem {
             ret.append(" ");
         }
         if (incSpeed > 0) {
-            ret.append("Gives Speed: ");
+            ret.append("Speed +");
             ret.append(incSpeed);
             ret.append(" ");
         }
         if (incJump > 0) {
-            ret.append("Gives Jump: ");
+            ret.append("Jump +");
             ret.append(incJump);
             ret.append(" ");
         }
         if (incPAD > 0) {
-            ret.append("Gives Attack: ");
+            ret.append("ATT +");
             ret.append(incPAD);
             ret.append(" ");
         }
         if (incMAD > 0) {
-            ret.append("Gives Magic Attack: ");
+            ret.append("Magic ATT +");
             ret.append(incMAD);
             ret.append(" ");
         }
-        if (incPDD > 0) {
-            ret.append("Gives Defense: ");
+        if (incPDD > 0 && incPDDr < 0) {
+            ret.append("DEF +");
             ret.append(incPDD);
             ret.append(" ");
         }
         if (incMDD > 0) {
-            ret.append("Gives Magic Defense: ");
+            ret.append("Magic DEF +");
             ret.append(incMDD);
             ret.append(" ");
         }
-        if (incSTRr > 0) {
-            ret.append("Gives STR %: ");
+        if (incSTRr > 0 && incLUKr <= 0 && incDEXr <= 0 && incINTr <= 0) {
+            ret.append("STR ");
             ret.append(incSTRr);
+            ret.append("%");
             ret.append(" ");
         }
-        if (incDEXr > 0) {
-            ret.append("Gives DEX %: ");
+        if (incDEXr > 0 && incLUKr <= 0 && incSTRr <= 0 && incINTr <= 0) {
+            ret.append("DEX ");
             ret.append(incDEXr);
+            ret.append("%");
             ret.append(" ");
         }
-        if (incINTr > 0) {
-            ret.append("Gives INT %: ");
+        if (incINTr > 0 && incLUKr <= 0 && incDEXr <= 0 && incSTRr <= 0) {
+            ret.append("INT ");
             ret.append(incINTr);
+            ret.append("%");
             ret.append(" ");
         }
-        if (incLUKr > 0) {
-            ret.append("Gives LUK %: ");
+        if (incLUKr > 0 && incSTRr <= 0 && incDEXr <= 0 && incINTr <= 0) {
+            ret.append("LUK ");
             ret.append(incLUKr);
+            ret.append("%");
             ret.append(" ");
         }
         if (incACCr > 0) {
-            ret.append("Gives ACC %: ");
+            ret.append("ACC ");
             ret.append(incACCr);
+            ret.append("%");
             ret.append(" ");
         }
         if (incEVAr > 0) {
@@ -237,38 +265,65 @@ public class StructPotentialItem {
             ret.append(" ");
         }
         if (incPADr > 0) {
-            ret.append("Gives Attack %: ");
+            ret.append("ATT ");
             ret.append(incPADr);
+            ret.append("%");
             ret.append(" ");
         }
         if (incMADr > 0) {
-            ret.append("Gives Magic Attack %: ");
+            ret.append("Magic ATT ");
             ret.append(incMADr);
+            ret.append("%");
             ret.append(" ");
         }
         if (incPDDr > 0) {
-            ret.append("Gives Defense %: ");
+            ret.append("DEF ");
             ret.append(incPDDr);
+            ret.append("%");
             ret.append(" ");
         }
         if (incMDDr > 0) {
-            ret.append("Gives Magic Defense %: ");
+            ret.append("Magic DEF ");
             ret.append(incMDDr);
+            ret.append("%");
             ret.append(" ");
         }
         if (incCr > 0) {
-            ret.append("Gives Critical %: ");
+            ret.append("Critical ");
             ret.append(incCr);
+            ret.append("%");
             ret.append(" ");
         }
         if (incDAMr > 0) {
-            ret.append("Gives Total Damage %: ");
+            ret.append("Total Damage ");
             ret.append(incDAMr);
+            ret.append("%");
             ret.append(" ");
         }
         if (level > 0) {
             ret.append("Level: ");
             ret.append(level);
+            ret.append(" ");
+        }
+        if(incSTRr > 0 && incINTr > 0 && incLUKr > 0 && incDEXr > 0)
+        {
+            isAllStat = true;
+            ret.append("All Stat ");
+            ret.append(incSTRr);
+            ret.append("%");
+            ret.append(" ");
+        }
+        if(incSTR > 0 && incLUK > 0 && incDEX > 0 && incINT > 0)
+        {
+            ret.append("All Stat +");
+            ret.append(incSTR);
+            ret.append(" ");
+        }
+        if(incCriticaldamageMin > 0 || incCriticaldamageMax > 0)
+        {
+            ret.append("Critical Damage ");
+            ret.append(incCriticaldamageMin);
+            ret.append("%");
             ret.append(" ");
         }
         return ret.toString();
